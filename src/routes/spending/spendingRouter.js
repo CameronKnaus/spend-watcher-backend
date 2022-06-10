df2454dd884c88c87b7e46ecdb42870bae11f996
cameronknaus <cdknaus@gmail.com>
2022-06-10 18:10:13 -0400

    Add edit transaction flow

--- src/routes/spending/spendingRouter.js
+++ src/routes/spending/spendingRouter.js
@@ -7,5 +7,6 @@ const verifyAuthToken = require('../../middleware/verifyAuthToken');
 spendingRouter.get('/summary', verifyAuthToken, require('./implementations/spendingSummary'));
 spendingRouter.get('/recent', verifyAuthToken, require('./implementations/recentSpending'));
 spendingRouter.post('/new', verifyAuthToken, require('./implementations/newSpending'));
+spendingRouter.post('/edit', verifyAuthToken, require('./implementations/editSpending'));
 
 module.exports = spendingRouter;
