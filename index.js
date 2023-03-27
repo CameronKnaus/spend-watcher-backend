606ed79057d8626c0727dc29c4a50b6e8e006b61
cameronknaus <cdknaus@gmail.com>
2023-03-27 09:51:34 -0400

    Add trips end points

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-require("dotenv").config({ path: './src/lib/variables.env' });
+require('dotenv').config({ path: './src/lib/variables.env' });
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+const dayjs = require('dayjs');
+const isBetween = require('dayjs/plugin/isBetween');
 const { DEV_MODE, PROD_DOMAIN, LOCAL_DOMAIN } = require('./src/lib/ENVIRONMENT_SETTINGS.json');
+
+// Route imports
+const authRoutes = require('./src/routes/authentication/authRouter'); // Routes concerning authentication
+const spendingRoutes = require('./src/routes/spending/spendingRouter');
+const recurringSpendingRoutes = require('./src/routes/recurringSpending/recurringSpendingRouter');
+const accountRoutes = require('./src/routes/accounts/accountsRouter');
+const tripRoutes = require('./src/routes/trips/tripsRouter');
 
 let allowedOrigin = PROD_DOMAIN;
 if (DEV_MODE) {
-    console.log("DEV MODE IS ON");
+    console.log('DEV MODE IS ON');
     allowedOrigin = LOCAL_DOMAIN;
     console.log(`Listening for requests from ${allowedOrigin}`);
 }
+
+// Augment dayjs with plugins
+dayjs.extend(isBetween);
 
 // Define the express app
 const app = express();
@@ -19,15 +31,14 @@ app.use(cookieParser());
 
 // Cross Origin Resource Sharing (CORS) settings
 app.use((req, res, next) => {
-    res.set("Access-Control-Allow-Origin", allowedOrigin);
-    res.set("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-    res.set("Access-Control-Request-Method: GET, POST");
-    res.set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-token, Authorization");
+    res.set('Access-Control-Allow-Origin', allowedOrigin);
+    res.set('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    res.set('Access-Control-Request-Method: GET, POST');
+    res.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-token, Authorization');
     res.set('Access-Control-Allow-Credentials', 'true');
     if (req.method === 'OPTIONS') {
         res.end();
-    }
-    else {
+    } else {
         next();
     }
 });
@@ -39,22 +50,17 @@ app.use(express.json());
 app.use(helmet());
 
 // Add routes
-const authRoutes = require('./src/routes/authentication/authRouter'); // Routes concerning authentication
 app.use('/api/auth', authRoutes);
-
-const spendingRoutes = require('./src/routes/spending/spendingRouter');
 app.use('/api/spending', spendingRoutes);
-
-const recurringSpendingRoutes = require('./src/routes/recurringSpending/recurringSpendingRouter');
 app.use('/api/recurring', recurringSpendingRoutes);
-
-const accountRoutes = require('./src/routes/accounts/accountsRouter');
 app.use('/api/accounts', accountRoutes);
+app.use('/api/accounts', accountRoutes);
+app.use('/api/trips', tripRoutes);
 
 // Define the port
 const PORT = process.env.PORT || 4000;
 
 // Starting our server.
 app.listen(PORT, () => {
-    console.log('Listening on port ' + PORT);
+    console.log(`Listening on port ${PORT}`);
 });
